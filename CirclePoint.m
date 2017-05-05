acd51a0166bfe9a3659b8e113e1019122848ecc8
GuoWenhao1996 <1842297753@qq.com>
2017-05-05 16:06:13 +0800

    上传实验报告内容

--- CirclePoint.m
+++ CirclePoint.m
@@ -2,6 +2,7 @@ function CirclePoint(x,y)
 % CirclePoint(x,y)   八点法算法绘圆
 % x                    横坐标
 % y                    纵坐标
+hold on
 plot(x,y,'.r');
 plot(y,x,'.r');
 plot(-x,y,'.r');
@@ -10,6 +11,7 @@ plot(x,-y,'.r');
 plot(-y,x,'.r');
 plot(-x,-y,'.r');
 plot(-y,-x,'.r');
+hold off
 grid on;
 xlabel('x轴线');
 ylabel('y轴线');
